a208546c7cb67563c557d4824c8aa9b53574caca
Nick Matvyeyev <mnasoft@gmail.com>
2016-03-21 22:54:24 +0200

    Добавлена фунция парсинга отдельного списка

--- razmernost.lisp
+++ razmernost.lisp
@@ -168,7 +168,8 @@
     (rez nil))
    ((equal len 0) (reverse rez))
     (setf sub
-  	  (cl-ppcre:scan-to-strings "(\\()|(\\))|(\\*)|(\\/)|(\\^)|([A-Za-z]+)|([+-]*[0-9]+)" str :start start)
+;;;;  	  (cl-ppcre:scan-to-strings "(\\()|(\\))|(\\*)|(\\/)|(\\^)|([A-Za-z]+)|([+-]*[0-9]+)" str :start start)
+  	  (cl-ppcre:scan-to-strings "(\\()|(\\))|(\\*)|(\\/)|(\\^)|(\\-)|(\\+)|([A-Za-z]+)|([0-9]+)" str :start start)
 	  len (length sub)
 	  start (+ start len)
 	  )
@@ -196,17 +197,6 @@
 	 (append (foo-rev (cdr l)) 
 		 (list (car l))))))
 
-(defun foo-max-operand-level(ll)
-  "Возвращает максимальный уровень операда, примененного в выражении"
-  (do* ((op-level 0) (len (length ll)) (i 0 (1+ i)) (tmp 0) (i-num 0))
-       ((>= i len) (values op-level i-num))
-    (break "1")
-    (setf tmp (nth i ll))
-    (break "~A" tmp)
-    (if (and (foo-operatorp tmp) (>= (cdr (foo-operatorp tmp)) op-level))
-	(setf op-level (cdr tmp)
-	      i-num i))))
-
 (defun foo-is-digit (str)
   (let
       ((rez (cl-ppcre:scan-to-strings "(^[+-]*[0-9]+)$" str)))
@@ -223,19 +213,61 @@
 
 (defun foo-is-dimension(str)
   )
+
+(defun foo-max-operand-level(ll)
+  "Возвращает максимальный уровень операда, примененного в выражении"
+  (do* ((op-level 0) (len (length ll)) (i 0 (1+ i)) (tmp 0) (i-num 0))
+       ((>= i len) (values i-num op-level))
+    (setf tmp (nth i ll))
+    (if (and (foo-operatorp tmp)
+	     (>= (cdr (foo-operatorp tmp)) op-level))
+	(setf op-level (cdr (foo-operatorp tmp))
+	      i-num i))))
+
+(defun foo-operand-op-operand(ll n)
+  (let
+      ((operand-l (1- n))
+       (operand-r (1+ n))
+       (operation n)
+       (len (length ll)))
+    (cond
+      ((and (< 0 n (1- len)))
+       (append
+	(subseq ll 0 operand-l)
+	(list
+	 (list
+	  (nth n ll)
+	  (nth operand-l ll)
+	  (nth operand-r ll)))
+	(subseq ll (1+ operand-r))))
+      (t ll))))
+
+(defun foo-parse-list(ll)
+  (do ()
+      ((= 1 (length ll)) (car ll))
+    (setf ll (foo-operand-op-operand ll (foo-max-operand-level ll)))))
+
+(defun foo-parse-list-recursive(ll)
+  (let ((a (car ll) (car ll)))
+   (cons
+   ((null ll) nil))));;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (declaim (optimize (debug 3)))
 
-(defparameter *ss* "kg^2*m*s*")
+(defparameter *ss* "25*kg^2+65*kg*g")
 
-(defparameter *tt* "(kg^-2)*(m*s^3)/(H^2*m^3)")
+(defparameter *tt* "(kg^(-2))*(m*s^3)/(H^2*m^3)")
 
-(defparameter *uu* "kg^2*m*s")
+(defparameter *uu* "kg^2")
 
-;;;;(foo-rev(foo-lexem-tree (foo-split *uu*)))
+(defparameter *ll* (foo-rev (foo-lexem-tree(foo-split *tt*))))
 
-;;;;(foo-convert-str-to-atom "/")
+(defparameter *oo* '(0 1 2 3 4 5 6))
 
-;;;;(foo-max-operand-level (foo-rev(foo-lexem-tree (foo-split *ss*))))
+(foo-parse-list *ll*)
+
+
+
 
