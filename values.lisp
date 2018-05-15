758cf471768a98a83bd5aebc211861fd8c719829
U-ZORYA\namatv <mnasoft@gmail.com>
2018-05-15 16:40:59 +0300

    2018-5-15 16:40:46 KO11-118383

--- values.lisp
+++ values.lisp
@@ -52,20 +52,42 @@
   (format t "Введите выражение:")
   (read-line))
 
+(defun print-stack (stack)
+  (qi-sample)
+  (print-stack-clean)
+  (let ((i 0))
+    (mapc #'(lambda (el) (format t "X~A=~A~%" (incf i) el))
+	  (reverse stack))))
+
+(defun print-stack-clean ()
+  (loop for i from 0 to 10 do
+       (format t "~%")))
+
 (defun quantity-inetractive ()
   "Позволяет выполнить запуск интерактивного калькулятора"
-  (do* ((str-lst  nil (push str str-lst))
-	(str     (prompt-read-line) (prompt-read-line))
+  (do* ((rez-lst  nil)
 	(rez      nil)
-	(rez-lst  nil))
+	(str-lst  nil (push str str-lst))
+	(str     (progn (print-stack rez-lst) (prompt-read-line))
+		 (progn (print-stack rez-lst) (prompt-read-line))))
        ((string= str "exit")
 	(values (reverse str-lst)
 		(reverse rez-lst)))
     (cond
       ((string= ""      (string-trim " " str)))
       ((string= "help"  (string-trim " " str)) (help))
+      ((string= "clear" (string-trim " " str)) (setf rez-lst  nil))
+      ((string= "<>"   (string-trim " " str))
+       (let ((x1 (pop rez-lst))
+	     (x2 (pop rez-lst)))
+	 (push x1 rez-lst)
+	 (push x2 rez-lst)))
       ((string= "*" str) (push (vd* (pop rez-lst) (pop rez-lst)) rez-lst))
-      ((string= "/" str) (push (vd/ (pop rez-lst) (pop rez-lst)) rez-lst))
+      ((string= "/" str) (push
+			  (let ((x1 (pop rez-lst))
+				(x2 (pop rez-lst)))
+			    (vd/ x2 x1))
+			  rez-lst))
       ((string= "+" str) (push (vd+ (pop rez-lst) (pop rez-lst)) rez-lst))
       ((string= "-" str) (push (vd- (pop rez-lst) (pop rez-lst)) rez-lst))
       (t
@@ -81,3 +103,26 @@
   (quantity-inetractive))
 
 (export	'qi)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun qi-sample ()
+  (mapc
+   (lambda (el)
+     (format t "~A~%" el))
+   '(
+     "
+Команды:
+==============================
+exit  - выход из калькулятора;
+clear - очистка стека;
+flip  - 
+<>    - 
+============================="
+     "(25 kgf + 783.565 gf) / cm^2"
+     "70*kgf/(70*cm^2)"
+     "(0-1)*(55 m^2+45 mm^2)kgf/cm^2"
+     "(1/kg^2)*(m*s^3)/(N^2*m^3)"
+     "3600 r/h"
+     "2°+10'+55.4\"")))
