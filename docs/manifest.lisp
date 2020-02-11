cf13b27b467da263c5229f481779120efb1cb42d
Nick Matvyeyev <mnasoft@gmail.com>
2020-02-11 16:35:48 +0200

    Tue Feb 11 14:35:48 UTC 2020

---
+++ docs/manifest.lisp
@@ -0,0 +1,7 @@
+(:docstring-markup-format :scriba
+ :systems (:mnas-dim-value)
+ :documents ((:title "Mnas-Dim-Value"
+              :authors ("Nick Matvyeyev")
+              :output-format (:type :multi-html
+                              :template :minima)
+              :sources ("manual.scr"))))
