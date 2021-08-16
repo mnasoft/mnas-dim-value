4e5d9acd956da2598081be6a0ee8c9aeff93f08d
Nick Matvyeyev <mnasoft@gmail.com>
2021-08-16 16:54:11 +0300

    16 авг 2021 г. 13:54:10

--- docs/manifest.lisp
+++
@@ -1,8 +0,0 @@
-(:docstring-markup-format
-   :scriba
-   :systems (:MNAS-DIM-VALUE :MNAS-DIM-VALUE/DOCS)
-   :documents ((:title "Mnas-Dim-Value"
-	        :authors ("Nick Matvyeyev")
-	        :output-format (:TYPE :MULTI-HTML :TEMPLATE :MINIMA) 
-                :sources ("mnas-dim-value.scr" "mnas-dim-value-graph.scr")
-                )))
